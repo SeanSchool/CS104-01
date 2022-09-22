7a58b9583c25214fad38d5c2a3dd66ffa140ae9a
SeanSchool <114018607+SeanSchool@users.noreply.github.com>
2022-09-22 12:11:19 -0400

    Hello World Program - Version 2

--- helloWorld - Intro Problem Solving.py
+++ helloWorld - Intro Problem Solving.py
@@ -1,3 +1,15 @@
-
-#This is version 1 of my hello world program
-print("Hello, my name is Sean")
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Sean/Desktop/IntroProblemSoving - Python Tutorial.py =
+Hello, World!
+>>> 
+= RESTART: C:/Users/Sean/Desktop/IntroProblemSoving - Python Tutorial.py =
+Hello, my name is Sean
+>>> 
+= RESTART: C:/Users/Sean/Desktop/IntroProblemSoving - Python Tutorial.py =
+Hello, my name is Sean
+Hello, World!
+>>> 
+== RESTART: C:/Users/Sean/Desktop/helloWorld - Intro Problem Solving.py ==
+Hello my name is Sean F
